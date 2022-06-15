ae2b9c1786cf5ce6a055a93f17503424fc4736d5
functional-Kimgyutae <stoprabbit2004@gmail.com>
2022-06-15 09:10:47 +0900

    2022-06-15

---
+++ repeat/problem_15552.py
@@ -0,0 +1,6 @@
+from sys import stdin
+
+n = int(stdin.readline())
+for _ in range(n) :
+    a,b = map(int,stdin.readline().split())
+    print(a+b)
---
+++ repeat/problem_8393.py
@@ -0,0 +1,3 @@
+from sys import stdin
+n = int(stdin.readline())
+print(int(n*(n+1)/2))
