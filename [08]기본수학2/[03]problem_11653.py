f03cb8a040f16e16ac12c84f5df2636c3e224d2c
functional-Kimgyutae <stoprabbit2004@gmail.com>
2022-09-02 11:32:13 +0900

    up

---
+++ [08]기본수학2/[03]problem_11653.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+def factorization(x):
+    d = 2
+
+    while d <= x:
+        if x % d == 0:
+            print(d)
+            x = x / d
+        else:
+            d = d + 1
+
+num = int(stdin.readline().rstrip())
+
+factorization(num)
